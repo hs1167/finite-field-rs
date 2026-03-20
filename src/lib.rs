d33dafec1f1985d4fbd34fa2a54942b967ac8d14
hs1167 <254142453+hs1167@users.noreply.github.com>
2026-03-20 01:03:32 +0100

    feat: implement Fp (Goldilocks) safe wrapper with canonical projection and basic algebraic tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// Goldilocks field: 2^64 - 2^32 + 1
+// hexadecimal base: 0xffffffff00000001 with '0x' prefixe for hexadecimal base
+pub const MOD: u64 = 0xffff_ffff_0000_0001; 
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Fp {
+    value: u64, // without pub to ensure the safety of wrapper
+}
+
+impl Fp {
+    // canonical projection  p : Z -> Fp
+    pub fn new(val: u64) -> Self {
+        Self {
+            value: val % MOD,
+        }
+    }
+
+    
+    pub fn value(self) -> u64 { // getter / read only
+        self.value
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // Basic algebraic tests
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_creation_and_modulo() {
+
+        let a = Fp::new(100);
+        assert_eq!(a.value(), 100);
+
+        let b = Fp::new(MOD);
+        assert_eq!(b.value(), 0);
+
+        let c = Fp::new(MOD + 1);
+        assert_eq!(c.value(), 1);
     }
 }
